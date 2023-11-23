9a4223339480a4c87606542bc6a821255bea92b7
focandlol <focandlol@naver.com>
2023-11-23 11:58:58 +0900

    sec01

---
+++ jpausesec/src/main/java/jpabook/jpausesec/JpausesecApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpausesec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpausesecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpausesecApplication.class, args);
+	}
+
+}
---
+++ jpausesec/src/test/java/jpabook/jpausesec/JpausesecApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpausesec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpausesecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
